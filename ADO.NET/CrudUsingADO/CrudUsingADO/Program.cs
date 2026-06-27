3289f8a719a4a4dc997f60fba11cc7a2aa7541e5
Abdul Haseeb <haseeb1389446@gmail.com>
2026-06-27 06:38:07 +0500

    Asp.Net

---
+++ ADO.NET/CrudUsingADO/CrudUsingADO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+public class AdoPractice
+{
+    public static void Main()
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Server=(localdb)\\MSSQLLocalDB; Database=ado; Trusted_Connection=True;"; 
+
+        con.Open();
+        Console.WriteLine("Connection Successful!");
+        con.Close();
+    }
+}
